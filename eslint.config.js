20874f507c626f98d0eee0dc68472d68d5db6ac2
Andres DCIC <asalamanca36@gmail.com>
2025-09-02 18:17:45 -0300

    [update] eslint

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ export default [
     },
   },
   {
-    files: ["test/**/*.js", "**/*.test.js"],
+    files: ["test/**/*.js", "**/*.test.js", "test.js"], 
     languageOptions: {
       globals: {
         describe: true,
