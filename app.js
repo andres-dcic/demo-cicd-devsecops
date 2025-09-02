1618b3e750f294b837898476f0ff01d9c7c76641
Andres DCIC <asalamanca36@gmail.com>
2025-09-02 18:28:17 -0300

    [update] eslint

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 // https: //github.com/expressjs/method-override#custom-logic
-app.use(methodOverride(function (req, res) {
+app.use(methodOverride(function (req, res) { // eslint-disable-line no-unused-vars
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
         // look in urlencoded POST bodies and delete it
         let method = req.body._method;
