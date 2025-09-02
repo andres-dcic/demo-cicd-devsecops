6703098808343332fbb08f7c284df5bbe65f689c
Andres DCIC <asalamanca36@gmail.com>
2025-09-02 17:58:33 -0300

    [update] eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,29 @@
-module.exports = [
+import js from "@eslint/js";
+import security from "eslint-plugin-security";
+
+export default [
+  js.configs.recommended,
   {
     files: ["**/*.js"],
-    plugins: {
-      security: require("eslint-plugin-security"),
-    },
-    extends: ["eslint:recommended"],
+    plugins: { security },
     rules: {
       // Puedes agregar reglas personalizadas aquí
+    },
+  },
+  {
+    files: ["test/**/*.js", "**/*.test.js"],
+    languageOptions: {
+      globals: {
+        describe: true,
+        it: true,
+        before: true,
+        after: true,
+        beforeEach: true,
+        afterEach: true,
+        console: true,
+        require: true,
+        module: true,
+      },
     },
   },
 ];
